bb98cc1398aa6aaf0734d3cb648f70412b58fc3c
Magnar Sveen <magnars@gmail.com>
2017-08-29 09:44:47 +0200

    Release 0.4.0

--- project.clj
+++ project.clj
@@ -1,5 +1,8 @@
-(defproject datoms-differ "0.3.0"
+(defproject datoms-differ "0.4.0"
   :description "Find the diff between two txes in datoms."
+  :url "http://github.com/magnars/datoms-differ"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[medley "0.8.4"]]
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.9.0-alpha14"]]
                    :plugins [[com.jakemccrary/lein-test-refresh "0.17.0"]]}})
