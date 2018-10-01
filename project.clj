116083b88fc62e5b0fc382ce1a10a9ec4afc54cb
Magnar Sveen <magnars@gmail.com>
2018-10-01 15:29:41 +0200

    Release 0.4.3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject datoms-differ "0.4.2"
+(defproject datoms-differ "0.4.3"
   :description "Find the diff between two txes in datoms."
   :url "http://github.com/magnars/datoms-differ"
   :license {:name "Eclipse Public License"
