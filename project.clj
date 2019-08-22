a9af038a8676b26054869861a16d1a3a3be81673
Magnar Sveen <magnars@gmail.com>
2019-08-22 14:16:33 +0200

    Bump dependencies

--- project.clj
+++ project.clj
@@ -3,11 +3,10 @@
   :url "http://github.com/magnars/datoms-differ"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[medley "0.8.4"]
+  :dependencies [[medley "1.2.0"]
                  [clansi "1.0.0"]]
-  :profiles {:dev {:dependencies [[org.clojure/clojure "1.10.0"]
-                                  [org.clojure/tools.cli "0.4.1"] ;; for kaocha to recognize command line options
-                                  [lambdaisland/kaocha "0.0-389"]
-                                  [kaocha-noyoda "2019-01-29"]]
-                   :plugins [[com.jakemccrary/lein-test-refresh "0.17.0"]]}}
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.10.1"]
+                                  [org.clojure/tools.cli "0.4.2"] ;; for kaocha to recognize command line options
+                                  [lambdaisland/kaocha "0.0-529"]
+                                  [kaocha-noyoda "2019-06-03"]]}}
   :aliases {"kaocha" ["run" "-m" "kaocha.runner"]})
