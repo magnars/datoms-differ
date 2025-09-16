910e3dbbd11817a06756917f836f8fdd320856a9
Magnar Sveen <magnars@gmail.com>
2025-09-16 14:53:37 +0200

    Bump persistent-sorted-set dependency to 0.3.0

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[dev.weavejester/medley "1.9.0"]
                  [clansi "1.0.0"]
-                 [persistent-sorted-set "0.1.2"]]
+                 [persistent-sorted-set "0.3.0"]]
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.10.1"]
                                   [org.clojure/tools.cli "0.4.2"] ;; for kaocha to recognize command line options
                                   [lambdaisland/kaocha "0.0-590"]
